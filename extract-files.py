8a8dc29b6c17e5a33c3a04491a4c9279b948ba19
Woomymy <woomy@woomy.be>
2023-12-08 17:13:49 +0000

    gold: Link media.c2 service against v33 libstagefright_foundation
    * Fixes crashes and high CPU usage
    
    Change-Id: I60c07d10c2db9335d20c6dbaaaeacc9313aebf06
    Signed-off-by: Woomymy <woomy@woomy.be>

--- extract-files.py
+++ extract-files.py
@@ -22,6 +22,8 @@ namespace_imports = [
 ]
 
 blob_fixups: blob_fixups_user_type = {
+    'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b': blob_fixup()
+        .add_needed('libstagefright_foundation-v33.so'),
     'vendor/bin/hw/android.hardware.security.keymint@1.0-service.mitee': blob_fixup()
         .add_needed('android.hardware.security.rkp-V1-ndk.so'),
     'vendor/etc/init/android.hardware.media.c2@1.2-mediatek-64b.rc': blob_fixup()
