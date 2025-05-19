49be5b2760679681a155e9d3809c7bb674219541
LuK1337 <priv.luk@gmail.com>
2025-05-19 23:01:52 +0700

    gold: Build Lineage common lights HAL
    Change-Id: Ia7610b2bd5de23407bbd9d7e39db67ce46174d03

--- device.mk
+++ device.mk
@@ -135,6 +135,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light-service.lineage
+
 # Media
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/media,$(TARGET_COPY_OUT_VENDOR)/etc)
