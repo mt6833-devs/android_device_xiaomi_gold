cbc8431bbf48b3ec4054bf8d731b2aa17dd70f1d
claxten10 <claxten10@gmail.com>
2025-11-15 21:46:44 +0000

    gold: Configure Thermal stack
    * Use Pixel Thermal HAL instead of the prebuilt MTK Thermal HAL.
    * Pull and configure mi_thermald for throttling.
    * Don't pull MTK thermal daemons as they are unneeded for us (also 32-bit).
    
    Change-Id: I6a7cc33364000342caf189e59ded5509cbe43807
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -207,6 +207,16 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/mediatek/libmtkperf_client \
     hardware/xiaomi
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal-service.mediatek
+
+PRODUCT_PACKAGES += \
+    init.mt6833.thermal.rc
+
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.mediatek \
