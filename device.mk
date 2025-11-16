bd361b23239ba11702f8db49df1d257ec09e1c6b
claxten10 <claxten10@gmail.com>
2025-11-16 18:14:32 +0000

    gold: Downrev composer service to 2.1
    Change-Id: Ib6870dd9a4b2de1863076901c19105f35496dc20
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -129,7 +129,7 @@ PRODUCT_COPY_FILES += \
 # Graphics
 PRODUCT_PACKAGES += \
     android.hardware.memtrack-service.mediatek \
-    android.hardware.graphics.composer@2.3-service
+    android.hardware.graphics.composer@2.1-service
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
